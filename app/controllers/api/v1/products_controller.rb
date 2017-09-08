89d80e472c4ee24721ca2a0ebfe3fd790762daa0
marioguio <mario_guio@outlook.com>
2017-09-08 11:48:50 -0500

    adding the function of creating products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::ProductsController < ApplicationController
     products = Product.all
     render json: products
   end
+
+  def new
+    product = Product.new
+  end
+  def create
+    product = Product.new(products_params)
+    if product.save
+      render json: product, status: 201
+    else
+      render json: {errors: product.errors}, status: 422
+    end
+  end
+
+private
+ def products_params
+   params.require(:product).permit(:name, :price)
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  
   namespace :api, defaults: { format: :json } do
   namespace :v1 do
     resources :products
